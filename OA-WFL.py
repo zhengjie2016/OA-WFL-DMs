a55d50a15c68645c6bb73c38332ab428393f0d64
zhengjie2016 <zjtx_2008@163.com>
2024-12-01 10:27:55 +0800

    Add files via upload

---
+++ OA-WFL.py
@@ -0,0 +1,73 @@
+import collections
+import copy
+from tqdm import tqdm
+import torch
+
+from training import evaluate_model
+from diffusion import diffusion_loss
+
+#channel # channel response vector
+#sigma # nosie power
+#eta # de-nosing factor of receive
+#p # transmit power of device
+
+
+def weighted_averaging(w_list, num_sample_list, dev_ind, channel, p, eta, sigma):
+    num_total_samples = sum(num_sample_list)
+    keys = w_list[0].keys()
+    w_avg = collections.OrderedDict()
+
+    device = w_list[0][list(keys)[0]].device
+
+    for k in keys:
+        w_avg[k] = torch.zeros(w_list[0][k].size()).to(device)  # Reshape w_avg to match local weights.
+
+    for k in keys:
+        for i in range(len(w_list)):
+            ind = dev_ind[i]
+            w_avg[k] += num_sample_list[i] * w_list[i][k] * abs(channel[k]) * np.sqrt(p[k])
+
+        w_avg[k] += torch.randn_like(w_avg[k]) * sigma  # Add noise after summation
+
+        w_avg[k] /= np.sqrt(eta[k])  # Divide by the de-noising factor eta[k]
+        w_avg[k] = torch.div(w_avg[k], num_total_samples)
+
+    return w_avg
+
+
+def local_update_fedavg(glob_model, client_loader, num_local_epochs, optim_args, test_loader=None):
+
+    local_model = copy.deepcopy(glob_model)
+    local_model.train()
+    device = local_model.device
+    optimizer = torch.optim.Adam(local_model.parameters(), **optim_args)
+    grad_scaler = torch.cuda.amp.GradScaler()
+    
+    # Training.
+    for epoch in range(num_local_epochs):
+        for (x, y) in tqdm(client_loader, desc='epoch {}/{}'.format(epoch + 1, num_local_epochs)):
+            optimizer.zero_grad()
+            x = x.to(device)
+            y = y.to(device)
+
+            # Calculate loss.
+            loss = diffusion_loss(local_model, x, y)
+            grad_scaler.scale(loss).backward()
+            grad_scaler.step(optimizer)
+            grad_scaler.update()
+    
+    # Evaluating.
+    train_loss = evaluate_model(local_model, client_loader, diffusion_loss, tqdm_desc='local_train_loss')
+    
+    # Return update.
+    local_update_dict ={
+        'local_w': local_model.state_dict(),
+        'num_samples': len(client_loader.dataset),
+        'train_loss': train_loss,
+    }
+    
+    if test_loader:
+        test_loss = evaluate_model(local_model, test_loader, diffusion_loss, tqdm_desc='local_test_loss')
+        local_update_dict['test_loss'] = test_loss
+    
+    return local_update_dict
---
+++ README.md
@@ -0,0 +1,15 @@
+## OA-WFL for GMs
+
+Over-the-air wireless federated learning for diffusion model setting.
+
+
+
+Use ***diff\_benchmark.ipynb*** for benchmark training setting.
+
+Use ***diff\_fed.ipynb*** for federated learning setting, which should choose one of the FL.py, WFL.py, and OA-WFL.py.
+
+
+
+The diffusion implementation is referred from the following colab notebook.
+
+<https://colab.research.google.com/drive/1IJkrrV-D7boSCLVKhi7t5docRYqORtm3>
